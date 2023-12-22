337abc75bd2a4a611c698170c064ed52925d7d4f
Harshitha Reddy <jrm512@txstate.edu>
2023-12-22 07:23:49 -0600

    Initial commit

---
+++ WeCareAngular/src/app/coach/coachhome/coachhome.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coachhome',
+  templateUrl: './coachhome.component.html',
+  styleUrl: './coachhome.component.css'
+})
+export class CoachhomeComponent implements OnInit{
+  ngOnInit(): void {
+  }
+}
---
+++ WeCareAngular/src/app/coach/coachprofile/coachprofile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coachprofile',
+  standalone: true,
+  imports: [],
+  templateUrl: './coachprofile.component.html',
+  styleUrl: './coachprofile.component.css'
+})
+export class CoachprofileComponent {
+
+}
